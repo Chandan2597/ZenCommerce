145043c62c238a958e6d767435984e65ed81a718
Chandan Koiri <chandankoiri1997@gmail.com>
2026-03-27 21:09:29 +0530

    ecommerce project

---
+++ store/models/customer.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.hashers import make_password
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def register(self):
+        self.save()
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except Customer.DoesNotExist:
+            return False
+
+    def isExists(self):
+        return Customer.objects.filter(email=self.email).exists()
+
+    class Meta:
+        verbose_name_plural = "Customers"
